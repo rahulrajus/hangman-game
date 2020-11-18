18c3311dfe7da96ed25b6b3c4b930e4f77fc52b8
Rahul Rajan <rahul.rajanus@gmail.com>
2020-11-18 18:21:41 -0500

    play turn

--- read_words.py
+++ read_words.py
@@ -4,7 +4,6 @@ def read_words():
     Input: file: file
     Output: words: list
     '''
-    opened_file = open('words.txt', 'r')
     with open('words.txt') as f:
         flat_list=[word for line in f for word in line.split()]
     del flat_list[:2]
