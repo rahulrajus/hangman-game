11c24ad097572c7d66efb622523da645c7fad6b2
Rahul Rajan <rahul.rajanus@gmail.com>
2020-11-23 11:35:29 -0500

    change msg type

--- client.py
+++ client.py
@@ -3,25 +3,46 @@ import time
 HOST = '127.0.0.1'  # The server's hostname or IP address
 PORT = 8080        # The port used by the server
 
+def send_server(data):
+    s.sendall(bytes(data, encoding='utf-8'))
+    len_data = ord(s.recv(1))
+    if(len_data == 0):
+        len_word = ord(s.recv(1))
+        len_incorrect = ord(s.recv(1))
+        word_data = s.recv(len_word).decode('utf-8')
+        incorrect_data = s.recv(len_incorrect).decode('utf-8')
+        print(word_data)
+        print(incorrect_data)
+        print(len_data)
+        print(len_word)
+        print(len_incorrect)
+    else:
+        word_data = s.recv(len_data).decode('utf-8')
+        print(word_data)
+
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
-    s.sendall(b'y')
-    data = s.recv(1024)
-    print(data)
+    send_server(chr(0))
     time.sleep(1)
-    print("next")
-    s.sendall(b'u')
-    data = s.recv(1024)
-    print(data)
+    send_server(chr(1) + 'j')
     time.sleep(1)
-    print("next")
-    s.sendall(b't')
-    data = s.recv(1024)
-    print(data)
+    send_server(chr(1) + 't')
+    time.sleep(1)
+    send_server(chr(1) + 'z')
 
-    time.sleep(1)
-    print("next")
-    s.sendall(b'z')
-    data = s.recv(1024)
-    print(data)
+    # print("next")
+    # s.sendall(b'1j')
+    # data = s.recv(9).decode('utf-8')
+    # print(data)
+    # time.sleep(1)
+    # print("next")
+    # s.sendall(b'1t')
+    # data = s.recv(9).decode('utf-8')
+    # print(data)
+
+    # time.sleep(1)
+    # print("next")
+    # s.sendall(b'1z')
+    # data = s.recv(9).decode('utf-8')
+    # print(data)
 
--- server.py
+++ server.py
@@ -2,9 +2,24 @@
 
 import socket               # Import socket module
 from game import *
+import struct
+
+def send_client(conn, game_state):
+   send_msg = ""
+   if(game_state == GameStatus.GAME_OUT_OF_GUESSES):
+      msg = "You Lost!"
+      flag = chr(len(msg))
+      send_msg = bytes(flag+msg, encoding='utf-8')
+   else:
+      word_state = ''.join(game_state.state)
+      incorrect_guesses = ''.join(game_state.incorrect_guesses)
+      word_len = chr(len(word_state))
+      num_incorrect = chr(len(incorrect_guesses))
+      send_msg = bytes(chr(0) + word_len + num_incorrect + word_state + incorrect_guesses, encoding='utf-8')
+   conn.sendall(send_msg)   
 
 games = dict()
-s = socket.socket()         # Create a socket object
+s = socket.socket()         # Create a socket object`
 host = socket.gethostname() # Get local machine name
 port = 8080                # Reserve a port for your service.
 s.bind((host, port))        # Bind to the port
@@ -15,34 +30,40 @@ conn, addr = s.accept()     # Establish connection with client.
 
 while True:
    print('Got connection from', addr)
-   data = conn.recv(1024).decode('ascii')
-   print(addr)
-   print(data)
-   status = None
-   
-   if(addr in games):
-      game = games[addr]
-      status = game.play_turn(data)
-      str_state = ''.join(game.state)
-      send_data = bytes(str(status) + str_state, encoding='utf-8')
-      print('sent again')
-      conn.sendall(send_data)
+   data_len = ord(conn.recv(1))
+   if(data_len > 0):
+      guess = conn.recv(data_len).decode('utf-8')
+      print("GUESS",guess)
+      state = games[addr].play_turn(guess)
+      print(games[addr].state, state)
+      send_client(conn, games[addr])
    else:
-      if(data == 'y'):
-         print('y here')
-         gs = GameState('words.txt',1)
-         games[addr] = gs
-         str_state = ''.join(gs.state)
-         conn.sendall(bytes(str(GameStatus.GAME_START) + str_state, encoding='utf-8'))
-      else:
-         print("nah")
-         conn.sendall(b'uhh nothing')
+      games[addr] = GameState("words.txt")
+      send_client(conn, games[addr])
 
+   print(addr)
+   print(data_len)
 
+   status = None
 
-
-
-
+   
+   # if(addr in games):
+   #    game = games[addr]
+   #    status = game.play_turn(data)
+   #    str_state = ''.join(game.state)
+   #    send_data = bytes(str(status) + str_state, encoding='utf-8')
+   #    print('sent again')
+   #    conn.sendall(send_data)
+   # else:
+   #    if(data == 'y'):
+   #       print('y here')
+   #       gs = GameState('words.txt',1)
+   #       games[addr] = gs
+   #       str_state = ''.join(gs.state)
+   #       conn.sendall(bytes(str(GameStatus.GAME_START) + str_state, encoding='utf-8'))
+   #    else:
+   #       print("nah")
+   #       conn.sendall(b'uhh nothing')
 
    # conn.send(b'Thank you for connecting')
    # c.close()                # Close the connection
